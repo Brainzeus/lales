c9e1302ec2622e0d52d4acdfe53caeed1c40b7c1
Brainzeus <89934607+Brainzeus@users.noreply.github.com>
2023-07-16 20:59:06 +0300

    Add files via upload

---
+++ mon.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+
+./SRBMiner-MULTI --algorithm minotaurx --pool stratum-eu.rplant.xyz:7068 --wallet RVB5jcbcTXPKiLLS2x9w19N6CdEF8vZEZz --gpu-boost 6
